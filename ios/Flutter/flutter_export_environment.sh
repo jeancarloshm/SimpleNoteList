1372ed64a9981e51d1ba48422310e009f83b245b
Jeancarlos Herrera <jeancarlos.herrera113@gmail.com>
2022-12-22 23:12:56 -0400

    Connecting firebase to flutter app

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/jeancarlosherrera/Development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/jeancarlosherrera/repos/Flutter/taskList"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/jeancarlosherrera/repos/Flutter/taskList/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=6"
+export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/jeancarlosherrera/repos/Flutter/taskList/.dart_tool/package_config.json"
